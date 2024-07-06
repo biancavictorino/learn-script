0620b8b412bbe89ebbd28065a4d3a688aec24c84
biancavictorino <biancamarie.victorino@gmail.com>
2024-07-06 17:45:07 +0800

    added learners-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
 import FeaturedCategories from "./Components/FeaturedCategories"
+import Learners from "./Components/Learners"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <FeaturedCategories />
+      <Learners />
     </div>
     </>
   )
---
+++ src/Data/Learner.js
@@ -0,0 +1,19 @@
+import Learner1 from '../assets/learner-1.png'
+import Learner2 from '../assets/learner-2.png'
+
+export const Learner = [
+    {
+        id: 1,
+        image: Learner1,
+        title: 'From Beginner to Mobile App Developer',
+        story: '“I had zero coding experience before LearnScript. Now, thanks to the structured learning path and practical projects, I have built my own mobile app and landed a job as a junior developer!  LearnScript supportive community was invaluable for staying motivated and getting feedback on my work.”',
+        name: '-David Lee',
+    },
+    {
+        id: 2,
+        image: Learner2,
+        title: 'Data Science Enthusiast Turned Professional',
+        story: '"My background was in finance, but I was always fascinated by data science. LearnScript offered the perfect platform to learn the essential skills through their Data Science Specialization with R. Now, I am pursuing a career in data science at a leading research firm, thanks to the skills and knowledge I gained from LearnScript!"',
+        name: '-Sarah Jones',
+    }
+]
