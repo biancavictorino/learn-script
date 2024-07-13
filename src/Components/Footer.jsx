facb87c019cb867c378817d559d1f2d01d758d5d
biancavictorino <biancamarie.victorino@gmail.com>
2024-07-13 22:35:32 +0800

    modified footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='px-8 flex flex-col gap-8 py-10 bg-primary-color bg-opacity-10'>
-        <div>
+    <div className='px-8 flex flex-col gap-10 py-10 bg-primary-color bg-opacity-10 h-screen justify-center md:px-20 md:py-16 md:pb-20 md:flex-row md:h-auto'>
+        <div className='md:w-2/5'>
             <h1 className='text-2xl font-bold'>Learn<span className='text-primary-color'>Script.</span></h1>
         </div>
-        <div className='flex flex-col gap-8 text-primary-color'>
-            <div className='flex flex-col gap-2 w-auto'>
+        <div className='flex flex-col gap-10 text-primary-color md:flex-row md:w-3/5 md:justify-around'>
+            <div className='flex flex-col gap-3'>
                 <a href="" className='w-fit' >Courses</a>
                 <a href="" className='w-fit'>Pricing</a>
                 <a href="" className='w-fit'>TeachSrcipt</a>
                 <a href="" className='w-fit'>FAQs</a>
             </div>
 
-            <div className='flex flex-col gap-2'>
+            <div className='flex flex-col gap-3'>
                 <a href="" className='w-fit'>About Us</a>
                 <a href="" className='w-fit'>Contact Us</a>
                 <a href="" className='w-fit'>Download App</a>
             </div>
 
-            <div className='flex flex-col gap-2'>
+            <div className='flex flex-col gap-3'>
                 <a href="" className='w-fit'>Terms</a>
                 <a href="" className='w-fit'>Privacy and Policy</a>
                 <a href="" className='w-fit'>Help and Support</a>
